540c02999987228974bb3cb15aee48948bfa8457
miguelangel9 <almiguel@live.com.mx>
2015-07-21 22:13:54 -0500

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,17 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) { 
+models.Quiz.find(quizId).then( 
+   function(quiz) {
+     if (quiz) {	
+     req.quiz = quiz; 
+     next();
+     } else {next (new Error('No existe quizId=' + quizId));}
+   }
+ ).catch(function(error) { next(error);});
+};
+
 // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
