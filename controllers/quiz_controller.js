023d0cdea7e925f9e83245e169bd48d6281e41b4
miguelangel9 <almiguel@live.com.mx>
2015-07-31 21:25:19 -0500

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -72,6 +72,31 @@ var quiz = models.Quiz.build( req.body.quiz );
  )
 };
 
+// PUT /quizes/:id 
+exports.update = function(req, res) { 
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(
+  function(err){
+    if (err) {
+   res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+    } else {
+// save: guarda en DB los campos pregunta y respuesta de quiz
+      req.quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then(function(){ res.redirect('/quizes')});
+        }// Redirección HTTP (URL relativo) lista de preguntas
+  }
+ )
+};
+
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) { 
+  var quiz = req.quiz; //autoload se instancia de quiz
+  res.render('quizes/edit', { quiz: quiz, errors: []});
+};
 
 // GET /author 
 exports.author = function(req, res){
