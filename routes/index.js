240a927a3d509f56c7ca5cd694226c33fa260b0f
miguelangel9 <almiguel@live.com.mx>
2015-08-10 22:20:20 -0500

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con: quizId
 router.param('quizId', quizController.load); // autoload: quizId
+router.param('commentId', commentController.load); // autoload: commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);     //formulario login
@@ -33,5 +34,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
